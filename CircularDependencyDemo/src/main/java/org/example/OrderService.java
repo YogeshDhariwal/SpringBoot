c89255c31f3edcaa9ca36a140b828630db978c78
Yogesh <yogeshdhariwal@gmail.com>
2026-07-25 23:24:12 +0530

    Solving cirular dependency problem using @Lazy

--- CircularDependencyDemo/src/main/java/org/example/OrderService.java
+++ CircularDependencyDemo/src/main/java/org/example/OrderService.java
@@ -1,17 +1,25 @@
 package org.example;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderService {
   private   PaymentService paymentService;
-
-    public OrderService(PaymentService paymentService) {
+/*  By using lazy initialization we create a proxy of Payment service and then
+a bean of OrderService is created  hence the problem of circular dependency is resolve because the
+bean creation is the problem that we faced in circular dependency
+   @Lazy -   created bean when required
+    by default spring is eager
+    we can change the property and set all bean as Lazy and when we want eager initialization we
+    do  @Lazy(value = "false")
+*/
+    public OrderService(@Lazy PaymentService paymentService) {
         this.paymentService = paymentService;
     }
     public void placeOrder(){
         paymentService.pay();
-        getOrderDetails();
+//        getOrderDetails();
         System.out.println("order placed");
     }
     public void getOrderDetails(){
--- CircularDependencyDemo/src/main/java/org/example/PaymentService.java
+++ CircularDependencyDemo/src/main/java/org/example/PaymentService.java
@@ -1,17 +1,19 @@
 package org.example;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class PaymentService {
-//   private OrderService orderService ;
-//
-//    public PaymentService(OrderService orderService) {
-//        this.orderService = orderService;
-//    }
+   private OrderService orderService ;
+
+    public PaymentService(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     public void pay(){
         System.out.println("payment done");
-//        orderService.getOrderDetails();
+       orderService.getOrderDetails();
     }
 }
